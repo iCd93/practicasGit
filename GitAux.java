5e334c8645c516589e11a07de44984b0cd4fe00d
unknown <rocioarrebola95@hotmail.com>
2015-03-08 13:32:39 +0100

    push

--- GitAux.java
+++ GitAux.java
@@ -17,11 +17,14 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+	System.out.println("insertado por alumno2");	//modif alumno 2
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
      // modificaciones alumno 1
         System.out.println("Alumno 1 inserta esta sentencia");
+
+	System.out.println("alumno 2");; //insertado por alumno 2
     }
 }
